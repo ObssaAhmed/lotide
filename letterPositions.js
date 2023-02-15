84ff1c12064d2a3b22917b99b819c0f90d090d49
Obssa <obssa.ahmed@torontomu.ca>
2023-02-14 19:59:52 -0500

    Adding letterPositions.js.

---
+++ letterPositions.js
@@ -0,0 +1,34 @@
+const eqArrays = function(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+            return false;
+        }
+    }
+    return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+    if (eqArrays(arr1, arr2)) {
+        console.log(`Assertion Passed: ${arr1} === ${arr2}`);
+    } else {
+        console.log(`Assertion Failed: ${arr1} !== ${arr2}`);
+    }
+};
+
+const letterPositions = function(sentence) {
+    const results = {};
+    for (let i = 0; i < sentence.length; i++) {
+        const letter = sentence[i];
+        if (letter !== ' ') {
+            if (results[letter]) {
+                results[letter].push(i);
+            } else {
+                results[letter] = [i];
+            }
+        }
+    }
+    return results;
+};
