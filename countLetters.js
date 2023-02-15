49c92e70436c43ead15d5cb9c1e717052c0cac5f
Obssa <obssa.ahmed@torontomu.ca>
2023-02-14 19:54:23 -0500

    Adding countLetter.js.

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+        console.log(`Assertion Passed: ${actual} === ${expected}`);
+    } else {
+        console.log(`Assertion Failed: ${actual} !== ${expected}`);
+    }
+};
+
+const countLetters = function(sentence) {
+    const result = {};
+
+    for (const letter of sentence) {
+        if (letter !== ' ') {
+            if (result[letter]) {
+                result[letter] += 1;
+            } else {
+                result[letter] = 1;
+            }
+        }
+    }
+
+    return result;
+};
